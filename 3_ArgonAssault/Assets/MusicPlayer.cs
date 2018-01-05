2e08bbc484dd843293eca0ac929a0ce8bdb33d95
RadoslawKozaczuk <radoslaw.kozaczuk@finalproject.pl>
2018-01-05 16:39:22 +0100

    ArgonAssault - splash screen and music

---
+++ 3_ArgonAssault/Assets/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	[SerializeField] private float levelLoadDelay = 4f;
+
+	void Awake()
+	{
+		// this means don't destroy the object that this script is attached to
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		Invoke("LoadNextLevel", levelLoadDelay);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void LoadNextLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
