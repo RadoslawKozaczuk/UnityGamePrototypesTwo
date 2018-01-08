f8a971b5da43643b6211de9faa641511543a85a4
RadoslawKozaczuk <radoslaw.kozaczuk@finalproject.pl>
2018-01-08 22:09:34 +0100

    ArgonAssault - enemies added, bug fixing, project clean up

---
+++ 3_ArgonAssault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	void Awake()
+	{
+		// if more than one music player in scene destroy yourself
+		// Unity Singleton Pattern
+		if (FindObjectsOfType<MusicPlayer>().Length > 1)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			// this means don't destroy the object that this script is attached to
+			DontDestroyOnLoad(this);
+		}
+	}
+}
---
+++ 3_ArgonAssault/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	[SerializeField] private float levelLoadDelay = 4f;
+
+	void Start()
+	{
+		Invoke("LoadNextLevel", levelLoadDelay);
+	}
+
+	private void LoadNextLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
